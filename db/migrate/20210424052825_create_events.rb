37fd4cc6e8575947a3f02e35c6faf4c2cb13cbb4
takahiro-sekine <doctype.html.1021@gmail.com>
2021-04-24 14:49:27 +0900

    controller,model作成

--- db/migrate/20210424052825_create_events.rb
+++ db/migrate/20210424052825_create_events.rb
@@ -1,7 +1,10 @@
 class CreateEvents < ActiveRecord::Migration[6.0]
   def change
     create_table :events do |t|
-
+      t.text :title,         null:false
+      t.date :limit_date
+      t.string :body,        null:false
+      t.references :user 
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_04_24_051759) do
+ActiveRecord::Schema.define(version: 2021_04_24_052825) do
+
+  create_table "events", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.text "title", null: false
+    t.date "limit_date"
+    t.string "body", null: false
+    t.bigint "user_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["user_id"], name: "index_events_on_user_id"
+  end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "email", default: "", null: false

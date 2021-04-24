4c9dba2be3be4bb13bf74ecb11185be65a4a0b66
takahiro-sekine <doctype.html.1021@gmail.com>
2021-04-24 14:30:08 +0900

    モデルコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
   devise_for :users
-
+  root to:'events#index'
+  resources :events do
+    resources :comments, only: :create
+    collection do
+      get 'search'
+    end
+  end
 end
